edc9c1020eae822135b4ff737d168a570c3b1220
Burhozturk <31951806+Burhozturk@users.noreply.github.com>
2018-09-30 12:46:47 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double pi=3.14159;
+        double radius=6.5;
+        double perimeter=2*radius*pi;
+        double area=radius*radius*pi;
+
+        System.out.println("Perimeter er: " +perimeter);
+        System.out.println("Area er:" +area);
+    }
+}
